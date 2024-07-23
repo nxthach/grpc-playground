5636ea294dbb83120f93d3e55f5c574ee1846b78
Thach Nguyen <nxthach@gmail.com>
2024-07-23 17:06:04 +0700

    commit code sec12

---
+++ src/main/java/org/example/sec12/BankService.java
@@ -0,0 +1,68 @@
+package org.example.sec12;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import io.grpc.Context;
+import io.grpc.Status;
+import io.grpc.stub.ServerCallStreamObserver;
+import io.grpc.stub.StreamObserver;
+import org.example.models.sec12.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BankService.class);
+
+    @Override
+    public void getAccountBalance(
+            BalanceCheckRequest request,
+            StreamObserver<AccountBalance> responseObserver) {
+
+        var accountNumber = request.getAccountNumber();
+
+        var accountBalance = AccountBalance.newBuilder()
+                .setAccountNumber(accountNumber)
+                .setBalance(AccountRepository.getBalance(accountNumber))
+                .build();
+
+        ((ServerCallStreamObserver<AccountBalance>) responseObserver).setCompression("gzip");
+
+        responseObserver.onNext(accountBalance);
+        responseObserver.onCompleted();
+
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+
+        var accountNumber = request.getAccountNumber();
+        var requestedAmount = request.getAmount();
+        var balance = AccountRepository.getBalance(accountNumber);
+
+        if(requestedAmount > balance){
+            responseObserver.onError(Status.FAILED_PRECONDITION.asRuntimeException());
+            return;
+        }
+
+        for (int i = 0; i < (requestedAmount/10) && !Context.current().isCancelled(); i++) {
+            var money = Money.newBuilder().setAmount(10).build();
+
+            responseObserver.onNext(money);
+            LOGGER.info("Response Money : {}", money);
+
+            //
+            AccountRepository.deductAmount(accountNumber, 10);
+
+            //
+            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+        }
+
+        LOGGER.info("Streaming is completed");
+
+        responseObserver.onCompleted();
+
+    }
+
+}
---
+++ src/test/java/org/example/sec12/Lec01GzipCallOptionTest.java
@@ -0,0 +1,24 @@
+package org.example.sec12;
+
+import org.example.models.sec12.BalanceCheckRequest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec01GzipCallOptionTest extends AbstractTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BalanceCheckRequest.class);
+
+    @Test
+    public void gzipDemo(){
+        var request = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(1)
+                .build();
+
+        var response = this.bankBlockingStub
+                .withCompression("gzip")
+                .getAccountBalance(request);
+
+        LOGGER.info("{}", response);
+
+    }
+}
